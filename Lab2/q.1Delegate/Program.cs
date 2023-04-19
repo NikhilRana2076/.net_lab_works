fb491c8b9dcc1b61344ef21965a2aad55eb95319
Nikhil Rana <nikhilrana2076@gmail.com>
2023-04-19 18:19:36 +0545

    lab 2 added

---
+++ Lab2/q.1Delegate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DelegateAddition
+{
+    delegate int AddDelegate(int a, int b);
+
+    class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            int firstDigit = 5;
+            int secondDigit = 7;
+
+            AddDelegate addDelegate = new AddDelegate(Add);
+
+            int result = addDelegate(firstDigit, secondDigit);
+
+            Console.WriteLine($"The sum of {firstDigit} and {secondDigit} is {result}.");
+        }
+    }
+}
---
+++ Lab2/q5_DictionaryNIkhil/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program5
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> customers = new Dictionary<int, string>();
+
+            customers.Add(191811, "Archana");
+            customers.Add(191812, "Kritika");
+            customers.Add(191813, "Nikhil");
+            customers.Add(191814, "Prakash");
+            customers.Add(191815, "Rochak");
+
+            foreach (KeyValuePair<int, string> customer in customers)
+            {
+                Console.WriteLine("Customer ID: " + customer.Key + "  " + "Customer Name: " + customer.Value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
