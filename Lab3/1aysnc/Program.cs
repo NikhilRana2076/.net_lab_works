351806dbd57bbf88d7101aa69bf1798b08b3ed7d
Nikhil Rana <nikhilrana2076@gmail.com>
2023-05-01 19:52:24 +0545

    lab 3 added

---
+++ Lab3/1aysnc/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Starting long process...");
+
+        // Start the long process asynchronously
+        await LongProcessAsync();
+
+        Console.WriteLine("Long process finished.");
+    }
+
+    static async Task LongProcessAsync()
+    {
+        // Simulate a long process by waiting for 5 seconds
+        await Task.Delay(9000);
+    }
+}
---
+++ Lab3/2upperNikhil/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "My Name is NIKHIL and This is my Code.";
+
+        var upperCaseLetters = str.Where(c => char.IsUpper(c));
+
+        Console.WriteLine("Uppercase letters in the string:");
+        foreach (var letter in upperCaseLetters)
+        {
+            Console.Write(letter + " ");
+        }
+    }
+}
+
